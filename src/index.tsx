fab86a1ece1997a9fc646bff3a07b42550972fd8
matgd <matgd@users.noreply.github.com>
2025-11-09 14:52:44 +0100

    Inline MangoHud config editor and update UI controls
    - Move MangoHudConfigEditor from mangohud.py into main.py
    - Remove mangohud.py and its imports
    - Add async methods to Plugin for MangoHud preset management
    - Refactor frontend to use new MangoHud API methods
    - Replace old test UI with sliders and fields for MangoHud preset
      editing
    - Remove unused code and event listeners from frontend

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,10 @@ import {
   PanelSection,
   PanelSectionRow,
   Navigation,
-  staticClasses
+  staticClasses,
+  SliderField,
+  Dropdown,
+  TextField
 } from "@decky/ui";
 import {
   addEventListener,
@@ -16,113 +19,100 @@ import {
 import { useEffect, useState } from "react";
 import { FaClock } from "react-icons/fa";
 
-// import logo from "../assets/logo.png";
+const OFFSET_X_SLIDER_FIELD_RANGE = 15;
+const OFFSET_Y_SLIDER_FIELD_RANGE = 15;
+const OFFSET_X_BASE = 1212;
+const OFFSET_Y_BASE = -5;
 
-// This function calls the python function "add", which takes in two numbers and returns their sum (as a number)
-// Note the type annotations:
-//  the first one: [first: number, second: number] is for the arguments
-//  the second one: number is for the return value
-const add = callable<[first: number, second: number], number>("add");
+const mangohudUpsertTimePreset = callable<[
+  preset_number: number,
+  alpha: number,
+  background_alpha: number,
+  offset_y: number,
+  offset_x: number,
+  time_format: string,
+  position: string,
+], void>("mangohud_upsert_time_preset");
+const mangohudGetCurrentPresetData = callable<[preset_number: number], any>("mangohud_get_current_preset_data");
 
-// This function calls the python function "start_timer", which takes in no arguments and returns nothing.
-// It starts a (python) timer which eventually emits the event 'timer_event'
-const startTimer = callable<[], void>("start_timer");
-
-
-const mangohudConfigExists = callable<[], boolean>("mangohud_config_exists");
-const mangohudConfigContents = callable<[], string>("mangohud_config_contents");
 
 function Content() {
-  const [result, setResult] = useState<number | undefined>();
+  const [chosenPresetLoaded, setChosenPresetLoaded] = useState<boolean>(false);
 
-  const onClick = async () => {
-    const result = await add(Math.random(), Math.random());
-    setResult(result);
-  };
+  const [errorMsg, setErrorMsg] = useState<string>("");
+
+  const [currentPresetData, setCurrentPresetData] = useState<any>({});
+
+  const [preset, setPreset] = useState<number>(3);
+  const [alpha, setAlpha] = useState<number>(1.0);
+  const [backgroundAlpha, setBackgroundAlpha] = useState<number>(0.0);
+  const [offsetX, setOffsetX] = useState<number>(0);
+  const [offsetY, setOffsetY] = useState<number>(0);
+  const [timeFormat, setTimeFormat] = useState<string>("%H:%M");
+  const [position, setPosition] = useState<string>("top-right");
 
 
-  const [mangohudConfPresent, setMangohudConfPresent] = useState<boolean>(false);
-  const [mangohudConfContents, setMangohudConfContents] = useState<string>("");
+  const initialLoad = async () => {
+    try {
+      const curr = await mangohudGetCurrentPresetData(3);
+      setCurrentPresetData(curr);
 
-  async function updateMangoHudConfigState() {
-    const exists = await mangohudConfigExists();
-    setMangohudConfPresent(exists);
-    if (exists) {
-      const contents = await mangohudConfigContents();
-      setMangohudConfContents(contents);
-    } else {
-      setMangohudConfContents("");
+      if (curr.alpha) setAlpha(parseFloat(curr.alpha));
+      if (curr.background_alpha) setBackgroundAlpha(parseFloat(curr.background_alpha));
+      if (curr.offset_x) setOffsetX(parseInt(curr.offset_x));
+      if (curr.offset_y) setOffsetY(parseInt(curr.offset_y));
+      if (curr.time_format) setTimeFormat(curr.time_format);
+      setChosenPresetLoaded(true);
+    } catch (e) {
+      setErrorMsg(`Failed to load current preset data: ${e}`);
     }
   }
 
-  const onMangoConfigRefreshStateClick = async () => {
-    await updateMangoHudConfigState();
-  };
-
   useEffect(() => {
-    // On component mount, check for MangoHud config
-    updateMangoHudConfigState();
-  }, []);
+    setChosenPresetLoaded(false);
+    initialLoad().catch(e => {
+      setErrorMsg(`Error during initial load: ${e}`);
+    });
+  }, [preset])
+
+  if (errorMsg !== "") {
+    return (
+      <PanelSection title="MangoHud config">
+        <div>Error: {errorMsg}</div>
+      </PanelSection>
+    )
+  }
 
   return (
-    <div>
-      <PanelSection title="MangoHud config">
-        <PanelSectionRow>
-          <div>
-            {mangohudConfPresent ? '✓ MangoHud config found.' : '✗ MangoHud config not found.'}
-          </div>
-          <ButtonItem
-            layout="below"
-            onClick={onMangoConfigRefreshStateClick}
-          >
-            {"Refresh MangoHud config state"}
-          </ButtonItem>
-          {mangohudConfPresent &&
-            <pre style={{ maxHeight: '200px', overflowY: 'auto', backgroundColor: '#f0f0f0', padding: '10px', borderRadius: '5px' }}>
-              {mangohudConfContents}
-            </pre>
-          }
-        </PanelSectionRow>
-      </PanelSection>
+    <PanelSection title="MangoHud config">
+      <PanelSectionRow>
+        <SliderField label="Affected preset" min={1} max={6} step={1} value={preset} onChange={(v) => setPreset(v)} showValue={true} description="Change preset to overwrite" />
+      </PanelSectionRow>
 
-      <PanelSection title="Panel Section">
-        <PanelSectionRow>
-          <ButtonItem
-            layout="below"
-            onClick={onClick}
-          >
-            {result ?? "Add two numbers via Python"}
-          </ButtonItem>
-        </PanelSectionRow>
-        <PanelSectionRow>
-          <ButtonItem
-            layout="below"
-            onClick={() => startTimer()}
-          >
-            {"Start Python timer"}
-          </ButtonItem>
-        </PanelSectionRow>
-
-        {/* <PanelSectionRow>
-          <div style={{ display: "flex", justifyContent: "center" }}>
-            <img src={logo} />
-          </div>
-        </PanelSectionRow> */}
-
-        {/*<PanelSectionRow>
-          <ButtonItem
-            layout="below"
-            onClick={() => {
-              Navigation.Navigate("/decky-plugin-test");
-              Navigation.CloseSideMenus();
-            }}
-          >
-            Router
-          </ButtonItem>
-        </PanelSectionRow>*/}
-      </PanelSection>
-    </div>
-  );
+      {chosenPresetLoaded && (
+        <>
+          <PanelSectionRow>
+            <SliderField label="Text alpha" min={0} max={1} step={0.1} value={alpha} onChange={(v) => setAlpha(v)} showValue={true} description="Change text opacity" />
+          </PanelSectionRow>
+          <PanelSectionRow>
+            <SliderField label="Background alpha" min={0} max={1} step={0.1} value={backgroundAlpha} onChange={(v) => setBackgroundAlpha(v)} showValue={true} description="Change background opacity" />
+          </PanelSectionRow>
+          <PanelSectionRow>
+            <SliderField label="Offset X" min={OFFSET_X_BASE - OFFSET_X_SLIDER_FIELD_RANGE} max={OFFSET_X_BASE + OFFSET_X_SLIDER_FIELD_RANGE} showValue={true} step={1} value={offsetX} onChange={(v) => setOffsetX(v)} description="Adjust the placement on X axis" />
+          </PanelSectionRow>
+          <PanelSectionRow>
+            <SliderField label="Offset Y" min={OFFSET_Y_BASE - OFFSET_Y_SLIDER_FIELD_RANGE} max={OFFSET_Y_BASE + OFFSET_Y_SLIDER_FIELD_RANGE} showValue={true} step={1} value={offsetY} onChange={(v) => setOffsetY(v)} description="Adjust the placement on Y axis" />
+          </PanelSectionRow>
+          {/*<PanelSectionRow>
+            <Dropdown menuLabel="Clock position (not available yet)" rgOptions={[{ data: mangohudDefaultKV?.position, label: mangohudDefaultKV?.position }]} selectedOption={position} />
+          </PanelSectionRow>*/}
+          <PanelSectionRow>
+            <TextField label="Time format" value={timeFormat} onChange={(v) => {setTimeFormat(v.target.innerText)}} description="Set the time format (strftime)" />
+          </PanelSectionRow>
+        </>
+      )}
+    </PanelSection>
+  )
 };
 
 export default definePlugin(() => {
@@ -133,17 +123,17 @@ export default definePlugin(() => {
   // });
 
   // Add an event listener to the "timer_event" event from the backend
-  const listener = addEventListener<[
-    test1: string,
-    test2: boolean,
-    test3: number
-  ]>("timer_event", (test1, test2, test3) => {
-    console.log("Template got timer_event with:", test1, test2, test3)
-    toaster.toast({
-      title: "template got timer_event",
-      body: `${test1}, ${test2}, ${test3}`
-    });
-  });
+  // const listener = addEventListener<[
+  //   test1: string,
+  //   test2: boolean,
+  //   test3: number
+  // ]>("timer_event", (test1, test2, test3) => {
+  //   console.log("Template got timer_event with:", test1, test2, test3)
+  //   toaster.toast({
+  //     title: "template got timer_event",
+  //     body: `${test1}, ${test2}, ${test3}`
+  //   });
+  // });
 
   return {
     // The name shown in various decky menus
@@ -157,7 +147,7 @@ export default definePlugin(() => {
     // The function triggered when your plugin unloads
     onDismount() {
       console.log("Unloading")
-      removeEventListener("timer_event", listener);
+      // removeEventListener("timer_event", listener);
       // serverApi.routerHook.removeRoute("/decky-plugin-test");
     },
   };
